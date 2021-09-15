02ed161c74850419014e62c834401a966d8d899d
FjellsMemory <dg239@students.uni-freiburg.de>
2021-09-15 22:44:51 +0200

    in the middle of ex13.py and sleepy :)

---
+++ hardcode/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, dude, get, fucked, gonads = argv
+
+print("The script is called:", script)
+print("Your first variable is:", dude)
+print("Your second variable is:", get)
+print("Your third variable is:", fucked)
+print("Your fourth variable is:", gonads)
