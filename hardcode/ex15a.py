d1cf18ab489d2f09766771cb40a274e29113a9c8
Dustin Gould <dg239@students.uni-freiburg.de>
2021-09-20 15:59:02 +0200

    still building ex15a

--- hardcode/ex15a.py
+++ hardcode/ex15a.py
@@ -2,10 +2,9 @@ from sys import argv
 
 script, text_file, new_text = argv
 
-with open("ex15_sample.txt", "w") as edit:
+#txt = open(text_file)
 
+with open(text_file, "r") as edit:
     print(f"okay pal, i'm gonna open the file {text_file} for you.")
     print("here are the contents:  ")
     print(edit.read())
-
-
