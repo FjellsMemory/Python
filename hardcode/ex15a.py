51873c9f0cbb649328fc78823508a4b0f865d04d
Dustin Gould <dg239@students.uni-freiburg.de>
2021-09-20 16:29:39 +0200

    completion of ex15a, write txt to file via argv and input(), both :D

--- hardcode/ex15a.py
+++ hardcode/ex15a.py
@@ -5,9 +5,9 @@ script, text_file, new_text = argv
 txt = open(text_file)
 print(f"okay pal, i'm gonna open the file {text_file} for you.")
 print("here are the contents:  ")
-print("->", txt.read())
+print("  ->", txt.read())
 print(f"now... the text you gave via command line was:")
-print("-> ", new_text,"\n", "i'm now gonna write that to the file.")
+print("  -> ", new_text,"\n", "i'm now gonna write that to the file.")
 
 with open(text_file, "w") as edit:
     edit.write(f"{new_text}")
@@ -15,4 +15,12 @@ with open(text_file, "w") as edit:
 txt = open(text_file)
 print(f"changes made.  reopening {text_file} for you.")
 print("here are the contents now:  ")
-print("->", txt.read())
+print("  ->", txt.read())
+
+input1 = input("last effin step - input your own text here: -> ")
+with open(text_file, "w") as edit:
+    edit.write(f"{input1}")
+
+txt = open(text_file)
+print(f"changes made.  {text_file} now reads:")
+print("  ->", txt.read())
