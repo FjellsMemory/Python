559a1f1a222a35abb6d81a40a05e33aa53f25517
Dustin Gould <dg239@students.uni-freiburg.de>
2021-09-20 16:03:44 +0200

    refinement of 15a

--- hardcode/ex15a.py
+++ hardcode/ex15a.py
@@ -2,9 +2,17 @@ from sys import argv
 
 script, text_file, new_text = argv
 
-#txt = open(text_file)
+txt = open(text_file)
+print(f"okay pal, i'm gonna open the file {text_file} for you.")
+print("here are the contents:  ")
+print("\t", txt.read())
+print(f"now... the text you gave via command line was:\n")
+print(new_text, "i'm now gonna write that to the file.")
 
-with open(text_file, "r") as edit:
-    print(f"okay pal, i'm gonna open the file {text_file} for you.")
-    print("here are the contents:  ")
-    print(edit.read())
+with open(text_file, "w") as edit:
+    edit.write(f"{new_text}")
+
+txt = open(text_file)
+print(f"changes made.  reopening {text_file} for you.")
+print("here are the contents now:  ")
+print("\t", txt.read())
