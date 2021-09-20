7730163196eafc5f941d958673830ed78bc2386b
Dustin Gould <dg239@students.uni-freiburg.de>
2021-09-20 15:33:59 +0200

    beginning .py for inserting user input to new text file

--- hardcode/ex15a.py
+++ hardcode/ex15a.py
@@ -2,8 +2,10 @@ from sys import argv
 
 script, text_file, new_text = argv
 
-txt = open(text_file)
+with open("ex15_sample.txt", "w") as edit:
 
-print(f"okay pal, i'm gonna open the file {text_file} for you.")
-print("here are the contents:  ")
-print(txt.read)
+    print(f"okay pal, i'm gonna open the file {text_file} for you.")
+    print("here are the contents:  ")
+    print(edit.read())
+
+
