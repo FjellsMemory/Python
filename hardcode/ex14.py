64a5187313c2330ff88397e338ddcd92b651361e
FjellsMemory <dg239@students.uni-freiburg.de>
2021-09-25 08:06:11 +0200

    NOT SURE - conflict madness

--- hardcode/ex14.py
+++ hardcode/ex14.py
@@ -1,18 +1,13 @@
 from sys import argv
 
-script, mrormrs, user_name = argv
-prompt = '$ '
 script, mrormrs, user_name, occupation = argv
 prompt = '-> '
 
-print(f"Hi {mrormrs} {user_name}, I'm the {script} script.")
 print(f"Hi {mrormrs} {occupation} {user_name}, I'm the {script} script.")
 print(f"I'd like to ask you a few questions.")
-print(f"Do you like me, {mrormrs} {user_name}?")
 print(f"Do you like me, {mrormrs} {occupation} {user_name}?")
 likes = input(prompt)
 
-print(f"Where do you live {mrormrs} {user_name}?")
 print(f"Where do you live {mrormrs} {occupation} {user_name}?")
 lives = input(prompt)
 
@@ -20,7 +15,6 @@ print(f"What kind of computer do you have?")
 computer = input(prompt)
 
 print(f"""
-Alright, {mrormrs} {user_name}, so you said "{likes}" about liking me.
 Alright, {mrormrs} {occupation} {user_name}, so you said "{likes}" about liking me.
 You live in {lives}.  Not sure where that is.
 And you have a {computer} computer.  Nice.
