061a9fd89d7d3e838342214fb759095fce1f8083
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-06 18:26:23 +0200

    little mod to ex 15 after some time away

--- hardcode/ex15a.py
+++ hardcode/ex15a.py
@@ -6,7 +6,7 @@ txt = open(text_file)
 print(f"okay pal, i'm gonna open the file {text_file} for you.")
 print("here are the contents:  ")
 print("  ->", txt.read())
-print(f"now... the text you gave via command line was:")
+print("now... the text you gave via command line was:")
 print("  -> ", new_text,"\n", "i'm now gonna write that to the file.")
 
 with open(text_file, "w") as edit:
