c2278a614f65897744abf971de93c11a50aede3c
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-07 09:55:50 +0200

    finished typing e16.py

--- hardcode/ex16.py
+++ hardcode/ex16.py
@@ -2,9 +2,9 @@ from sys import argv
 
 script, filename = argv
 
-print("")
-print("")
-print("")
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
 
 input("?")
 
@@ -13,3 +13,21 @@ target = open (filename, "w")
 
 print("Truncating the file.  See ya!")
 target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line1:  ")
+line2 = input("line2:  ")
+line3 = input("line3:  ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
