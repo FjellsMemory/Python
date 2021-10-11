90e36bf1e0fa372af638c20da32d2ded323360c7
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-11 15:18:59 +0200

    quote close

--- hardcode/ex17.py
+++ hardcode/ex17.py
@@ -11,7 +11,7 @@ indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
 
-print(f"Does the output file exist? {exists(to_file)})
+print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 
 out_file = open(to_file, 'w')
