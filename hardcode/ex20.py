9be4f6b4671cf97edddd5212a01169b9604c9cb2
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-13 14:29:09 +0200

    commented ex20.py

--- hardcode/ex20.py
+++ hardcode/ex20.py
@@ -8,6 +8,11 @@ def print_all(f):
 def rewind(f):
     f.seek(0)
 
+# i was confused about the following function
+# but now i get it, line_count is just an int, passed explicitly
+# and readline() just prints a line from a file object - wherever the
+# "cursor" just happens to be.  in this case, f.seek(0) was called
+# beforehand, and so the cursor was at line 1
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
@@ -26,8 +31,8 @@ print("Let's print three lines.")
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
