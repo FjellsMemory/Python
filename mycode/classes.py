badc833eddc8c47eb9188e4600c30398dd35d761
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-14 09:08:38 +0200

    further work on classes.py

--- mycode/classes.py
+++ mycode/classes.py
@@ -5,13 +5,28 @@ class Homeboi:
 
     def pltest(self):
         if self.powerlevel > 9000:
-            print("His power level is over 9000!")
+            print(f"{self.name}'s power level is over 9000!")
         else:
-            print(f"His power level is only {self.poewerlevel}")
+            print(f"{self.name}'s power level is only {self.poewerlevel}")
+
+    def set_pl(self, pl):
+        self.powerlevel = pl
+    
 
 Jeff = Homeboi("Jeff", 9001)
 inst = 'Jeff = Homeboi("Jeff", 9001)'
 
 print(f"Creating a Homboi 'Jeff' as follows: '{inst}'")
-print("Performing powerlevel test on Jeff...")
+print("Performing powerlevel test on Jeff...\n")
 Jeff.pltest()
+
+print("\nNow create your own Homeboi...")
+tempname = input("Give your Homeboi a name:  ")
+temppl = int(input("Annnnnd a power level:  "))
+userobj = Homeboi(tempname, temppl)
+yesno = input(f"Want to check {tempname}'s powerlevel? (y/n?): ")
+if yesno.lower() == "y":
+    userobj.pltest()
+else:
+    pass
+print(userobj.name, userobj.powerlevel)
