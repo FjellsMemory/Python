abfeb7fc0ffa59c6db0a51a6d9418f08402a0a12
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-14 09:26:56 +0200

    even further tweaking of classes.py - return of the Homeboi

--- mycode/classes.py
+++ mycode/classes.py
@@ -7,11 +7,11 @@ class Homeboi:
         if self.powerlevel > 9000:
             print(f"{self.name}'s power level is over 9000!")
         else:
-            print(f"{self.name}'s power level is only {self.poewerlevel}")
+            print(f"{self.name}'s power level is only {self.powerlevel}")
 
     def set_pl(self, pl):
         self.powerlevel = pl
-    
+
 
 Jeff = Homeboi("Jeff", 9001)
 inst = 'Jeff = Homeboi("Jeff", 9001)'
@@ -24,8 +24,15 @@ print("\nNow create your own Homeboi...")
 tempname = input("Give your Homeboi a name:  ")
 temppl = int(input("Annnnnd a power level:  "))
 userobj = Homeboi(tempname, temppl)
+
 yesno = input(f"Want to check {tempname}'s powerlevel? (y/n?): ")
 if yesno.lower() == "y":
+    userobj.pltest()
+else:
+    pass
+yesno = input(f"Want to tweak {tempname}'s powerlevel? (y/n?): ")
+if yesno.lower() == "y":
+    userobj.set_pl(int(input("New powerlevel?:  ")))
     userobj.pltest()
 else:
     pass
