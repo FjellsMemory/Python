4f046ff90f3f7d6eb2e0715c107b6739f644c5cd
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-19 15:26:07 +0200

    testing

--- mycode/1ctof.py
+++ mycode/1ctof.py
@@ -2,3 +2,5 @@ print("Listen, fuckwad, I'm going to convert Celcius into Fahrenheit.")
 ctemp = input("Go ahead, give me a Celcius temperature.  Enter one here:  ")
 ftemp = float(ctemp) * 9/5 + 32
 print("K then, fuckstick, that temp in Fahrenheit is:  " + str(ftemp))
+
+#test change
