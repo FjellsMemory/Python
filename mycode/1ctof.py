beec780da746c9b446a7da007966e68f58c83131
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-20 06:10:07 +0200

    brushed up two old scripts via vsc reccomendations

--- mycode/1ctof.py
+++ mycode/1ctof.py
@@ -1,7 +1,9 @@
+"""This script converts Celsius to Fahrenheit."""
+
 print("Listen, fuckwad, I'm going to convert Celcius into Fahrenheit.")
 ctemp = input("Go ahead, give me a Celcius temperature.  Enter one here:  ")
 ftemp = float(ctemp) * 9/5 + 32
 print("K then, fuckstick, that temp in Fahrenheit is:  " + str(ftemp))
 
-#test change
-#another small test change
+# test change
+# another small test change
--- mycode/1multby5.py
+++ mycode/1multby5.py
@@ -1,6 +1,11 @@
-def multby5(input):
-    print(f"Okay, bro, your number times 5 is {int(input) * 5}")
+"""This script takes user input and mults it by 5 via a function."""
+
+
+def multby5(inp):
+    """Mult the passed argument by 5."""
+    print(f"Okay, bro, your number times 5 is {int(inp) * 5}")
     return
+
 
 myVar = input("Yo, bro, input a number: ")
 multby5(myVar)
