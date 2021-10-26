114cbd8a613765ed148740cb970ad51078889a04
FjellsMemory <dg239@students.uni-freiburg.de>
2021-10-26 12:41:48 +0200

    dickin around

--- mycode/1notepadscratch.py
+++ mycode/1notepadscratch.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from math import log2
+resa = int(log2(64)) + 2 ** abs(1+1j)
+print(f"res(a) ist {type(resa)}")
+
+from math import sqrt, floor, ceil
+resb = floor(2.3 * 7) * ceil(2 ** 3 + 7.1)
+print(f"res(b) ist {type(resb)}")
+
+from math import pi, sin, cos, radians
+resc = cos(pi/4)**2 + sin(radians(45))**2j
+print(f"res(c) ist {type(resc)}")
+
+resd = 6 * round(2.1, 1) // 1
+print(f"res(d) ist {type(resd)}")
