93bf2a966f4c0c34b978825d1298ec41203acbce
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-04 10:16:26 +0100

    scratch changes

--- mycode/scratch2.py
+++ mycode/scratch2.py
@@ -1,11 +1,10 @@
-"""Keepin up with Thiemann."""
+"""Keepin up with Thiemann.
 
 
 def test_result(
     max_points: int,
     percentage: int,
         points: int) -> str:
-    """Calculate whether some dude passes or fails."""
     passed = (points >= max_points * percentage / 100)
     if passed:
         return "pass"
@@ -16,3 +15,9 @@ def test_result(
 assert(test_result(100, 50, 50) == "pass")
 assert(test_result(100, 50, 30) == "fail")
 assert(test_result(100, 50, 70) == "pass")
+"""
+
+num = 4.0
+strnum = (str(num) + "00")
+print(len(str(num)))
+print(strnum)
