876a791335458d15cb7b5dbe5ae61d9ff9ea2edc
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-09 12:07:49 +0100

    just trying to get comfortable with zip function

--- mycode/scratch2.py
+++ mycode/scratch2.py
@@ -1,6 +1,5 @@
 """Keepin up with Thiemann.
 
-
 def test_result(
     max_points: int,
     percentage: int,
@@ -15,9 +14,23 @@ def test_result(
 assert(test_result(100, 50, 50) == "pass")
 assert(test_result(100, 50, 30) == "fail")
 assert(test_result(100, 50, 70) == "pass")
-"""
+
 
 num = 4.0
 strnum = (str(num) + "00")
 print(len(str(num)))
 print(strnum)
+
+"""
+
+mylist = (3, 4, 5, 6)
+crazy = "boom"
+ziplist = (list(zip(mylist, crazy)))
+for i in ziplist:
+    ind = ziplist.index(i)
+    ziplist[ind] = list(i)
+print(ziplist)
+
+mytup = (3, 'b')
+print(mytup)
+print(list(mytup))
