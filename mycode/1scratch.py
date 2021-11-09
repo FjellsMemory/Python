4566e35beea52bbad94470ebc55c3461b6ad7ee9
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-09 20:14:35 +0100

    scratchin around

--- mycode/1scratch.py
+++ mycode/1scratch.py
@@ -13,7 +13,7 @@ print(list)
 var = "homeboi is a {}"
 print(var.format("king"))
 """
-
+'''
 def strictly_positive(x):
     """Bitch, i'm a bus, comparing x to zero."""
     if x > 0 and x < 100:
@@ -23,4 +23,12 @@ def strictly_positive(x):
 if __name__ == "__main__":
     inp = input("enter a number:  ")
     strictly_positive(float(inp))
+'''
 
+def check_for_3(tup: tuple) -> tuple:
+    """Take tuple and return tuple only with mod 3 values."""
+    tup_of_3s = []
+    for n in tup:
+        if n % 3 == 0:
+            tup_of_3s = tup_of_3s + [n]
+    return tuple(tup_of_3s)
