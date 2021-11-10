d052118003b7839e3d08ea72f3ef8929c6faf661
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-10 18:19:33 +0100

    keeping deleteme.py alive - dubious decision

---
+++ mycode/uni-practice/deleteme.py
@@ -0,0 +1,27 @@
+"""Just another in a long line of deletable scratches - yet they're all cool."""
+from numbers import Number
+from typing import Sequence
+
+
+def product(xs: Sequence[Number]):
+    """Eff yer mom."""
+    result = 1
+    for x in xs:
+        result = result * x
+    return result
+
+
+def fact(n: int) -> int:
+    """Get bent."""
+    return product(range(1, n+1))
+
+
+def Dondlsfact(n: int) -> int:
+    """
+    Recurse the EFF out of n, mult-ing it by n-1 in recursive calls.
+    If something gets in your way... turn = once you hit n-1 = 0, return 1.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * Dondlsfact(n - 1)
