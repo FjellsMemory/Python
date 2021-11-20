d0b7e673d7b9709ac3b1f997b16962a9bdd63940
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-20 17:51:07 +0100

    erased lots of pics

--- mycode/uni-practice/foo.py
+++ mycode/uni-practice/foo.py
@@ -1,17 +1,22 @@
-def pixel_to_comp(z: complex, w: complex, x: int, y: int, rx: int, ry: int) -> complex:
-    """Return complex number assoc with a specific pixel value given x by y."""
-
-    axis_x = rx / 2
-    axis_y = rx / 2
+def sample(z: complex, w: complex, x: int, y: int, rx: int, ry: int) -> complex:
+    """Return complex number assoc with a specific pixel value on grid x, y.
+    scale = rx / ry
+    axis_x = rx / 2 #* (abs(z.real) - abs(w.real))
+    axis_y = ry / 2 #* (abs(z.imag) - abs(w.real))
     new_x_coor = x - axis_x
     new_y_coor = y - axis_y
 
-    xn = new_x_coor/axis_x
-    yn = new_x_coor/axis_y
+    ratio_x = (x) / rx
+    ratio_y = (y) / ry
+    final_x = ratio_x * (z.real + w.real)
+    final_y = ratio_y * (z.imag + w.imag)
 
-    mycomplex = complex(xn, yn)
+    xn = new_x_coor / axis_x#/ (abs(z.real) + abs(w.real))
+    yn = new_y_coor / axis_y#/ (abs(z.imag) + abs(w.imag))
+
+    mycomplex = complex(final_x, final_y)
     return mycomplex
-
+    """
 
 z = complex(-1, -1)
 w = complex(1, 1)
