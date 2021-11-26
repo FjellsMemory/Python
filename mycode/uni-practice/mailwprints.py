c53faa0353f6dc0e0091dec695d5479eb6133a71
FjellsMemory <dg239@students.uni-freiburg.de>
2021-11-26 08:02:33 +0100

    emptied delete list in correct tab row!

--- mycode/uni-practice/mailwprints.py
+++ mycode/uni-practice/mailwprints.py
@@ -44,8 +44,8 @@ def show_mail_address(address: MailAddress) -> str:
 
 def show_mail(mail: Mail) -> str:
     """Return the mail as a readable as string."""
-    return f"""\nFrom: {mail.sender.name}@{mail.sender.domain}
-To: {mail.receiver.name}@{mail.receiver.domain}
+    return f"""From: {show_mail_address(mail.sender)}
+To: {show_mail_address(mail.receiver)}
 Subject: {mail.subject}\n
 {mail.body}"""
 
@@ -54,25 +54,22 @@ def show_mail_account(account: MailAccount) -> str:
     """Return the mail's account attributes as string."""
     inboxstr = ""
     k = 1
-    for x in account.inbox:
-        inboxstr = inboxstr + f"""Mail #{k}:
-    sender: {x.sender.name}@{x.sender.domain}
-    receiver: {x.receiver.name}@{x.receiver.domain}
-    subject: {x.subject}
-    body: \n{x.body}\n
-    """
+    for mail in account.inbox:
+        inboxstr = inboxstr + f"""
+    Mail #{k}:\n
+{show_mail(mail)}\n
+"""
         k += 1
     outboxstr = ""
     k = 1
-    for x in account.outbox:
-        outboxstr = outboxstr + f"""Mail #{k}:
-    sender: {x.sender.name}@{x.sender.domain}
-    receiver: {x.receiver.name}@{x.receiver.domain}
-    subject: {x.subject}
-    body: \n{x.body}\n
-    """
+    for mail in account.outbox:
+        outboxstr = outboxstr + f"""
+    Mail #{k}:\n
+{show_mail(mail)}\n
+"""
         k += 1
     return f"""\nAccount Name: {account.name}\n
+
 ####################
     Inbox Items:
 ####################
@@ -83,13 +80,26 @@ def show_mail_account(account: MailAccount) -> str:
     Outbox Items:
 #####################
 
-    {print("") if outboxstr == "" else outboxstr}\n"""
+    {print("") if outboxstr == "" else outboxstr}"""
 
 
 def show_mail_server(server: MailServer) -> str:
     """Return the mail's server attributes as string."""
-    serverlist = ", ".join(str(x) for x in server.accounts)
-    return ", ".join([server.domain, serverlist])
+    accountstr = ""
+    k = 1
+    for acc in server.accounts:
+        accountstr = accountstr + f"""
+    Account #{k}
+{show_mail_account(acc)}\n
+"""
+        k += 1
+    return f"""\nServer Name: {server.domain}\n
+
+####################
+     Accounts:
+####################
+
+    {print("") if accountstr == "" else accountstr}"""
 
 
 def find_server(d_sought: str, ser_list: list[MailServer]) -> MailServer | None:
@@ -145,18 +155,18 @@ def deliver_all_mail(servers_list: list[MailServer]):
         print(f"\nThere are {len(servers_list)} server(s) in this list total.")
         print(f"Beginning my work on server {x.domain} at index[{servers_list.index(x)}] of the list now.")
         for y in x.accounts:
-            print(f"Starting a check for outbound emails in {y.name}'s account outbox now.")
+            print(f"\nStarting a check for outbound emails in {y.name}'s account outbox now.")
+            print(f"There is/are now currently {len(y.outbox)} mail(s) in this outbox.")
             for z in y.outbox:
-                print(f"There is/are now currently {len(y.outbox)} mail(s) in this outbox.")
                 print(f"The email I'm gonna verify and deliver now is titled {z.subject}.")
                 if y.name == z.sender.name and x.domain == z.sender.domain:
                     print(f"i'm about to stick {z.subject} into deliver_mail() as an argument and send it the fuck off.")
                     deliver_mail(z, servers_list)
                     deliver_count += 1
-                    print(f"So I just now delivered {z.subject} to {z.receiver.domain}, and now I'm gonna add it to list scheduled for deletion.")
+                    print(f"So I just delivered {z.subject} to {show_mail_address(z.receiver)}, and now I'm gonna add it to list scheduled for deletion.")
                     del_list = del_list + [z]
                     print("Added it to del_list.  Moving onto next loop - check which email is now at the top of the outbox stack!!!.")
-                    print(f"I've ostensibly now delivered {deliver_count} mails from {x.domain} to {z.receiver.domain}.")
+                    print(f"I've ostensibly now delivered {deliver_count} mails from {x.domain} to elsewhere.")
                 else:
                     print(f"Something about the address of {z.subject} is FUCKY.  I ain't sendin this shit, get rekt, spammer.")
                     fucky_list = fucky_list + [z]
@@ -176,21 +186,21 @@ def deliver_all_mail(servers_list: list[MailServer]):
                 print(f"Now going to delete {mail.subject} from da list.")
                 if mail in fucky_list:
                     y.outbox.remove(mail)
-        del_list = []
-        fucky_list = []
+            del_list = []
+            fucky_list = []
 
 
 if __name__ == "__main__":
     mailaddress1a = MailAddress("Me", "mymail.com")
     mailaddress1b = MailAddress("You", "yourmail.com")
     subject1: str = "Super important stuff!"
-    body1: str = "\t\tActually... it's not that important.\n\t\tSincerely,\n\t\tMe"
+    body1: str = "Actually... it's not that important.\n\nSincerely,\nMe"
     mail1 = Mail(mailaddress1a, mailaddress1b, subject1, body1)
 
     mailaddress2a = MailAddress("Me", "mymail.com")
     mailaddress2b = MailAddress("Him", "hismail.com")
     subject2: str = "Enuff is Enuff!"
-    body2: str = "\t\tWe gotta move these refrigerators.\n\t\tSincerely,\n\t\tDire Straits"
+    body2: str = "We gotta move these refrigerators.\n\nSincerely,\nDire Straits"
     mail2 = Mail(mailaddress2a, mailaddress2b, subject2, body2)
 
     inbox1 = []
@@ -200,13 +210,13 @@ if __name__ == "__main__":
     mailaddress3a = MailAddress("You", "yourmail.com")
     mailaddress3b = MailAddress("Him", "hismail.com")
     subject3: str = "McGruff the Crime Dog here!"
-    body3: str = "\t\tIf you see something...just walk away.\n\t\tSincerely,\n\t\tMcGruff"
+    body3: str = "If you see something...just walk away.\n\nSincerely,\nMcGruff"
     mail3 = Mail(mailaddress3a, mailaddress3b, subject3, body3)
 
     mailaddress4a = MailAddress("You", "yourmail.com")
     mailaddress4b = MailAddress("Me", "mymail.com")
     subject4: str = "Gosh i'm tired!"
-    body4: str = "\t\tGet rekt enjoy your holiday.\n\t\tSincerely,\n\t\tJoe"
+    body4: str = "Get rekt enjoy your holiday.\n\nSincerely,\nJoe"
     mail4 = Mail(mailaddress4a, mailaddress4b, subject4, body4)
 
     inbox2 = []
@@ -216,31 +226,44 @@ if __name__ == "__main__":
     mailaddress5a = MailAddress("Him", "hismail.com")
     mailaddress5b = MailAddress("Me", "mymail.com")
     subject5: str = "Comin for Ya!"
-    body5: str = "\t\tBasically gonna destroy your family.\n\t\tSincerely,\n\t\tVoltron"
+    body5: str = "Basically gonna destroy your family.\n\nSincerely,\nVoltron"
     mail5 = Mail(mailaddress5a, mailaddress5b, subject5, body5)
 
     mailaddress6a = MailAddress("Him", "hismail.com")
     mailaddress6b = MailAddress("You", "yourmail.com")
     subject6: str = "My name is Sue!"
-    body6: str = "\t\tHow do you do?\n\t\tSincerely,\n\t\tCash"
+    body6: str = "How do you do?\n\nSincerely,\nCash"
     mail6 = Mail(mailaddress6a, mailaddress6b, subject6, body6)
 
     inbox3 = []
     outbox3 = [mail5, mail6]
     mailaccount3 = MailAccount("Him", inbox3, outbox3)
 
+    mailaddress7a = MailAddress("She", "hismail.com")
+    mailaddress7b = MailAddress("Me", "mymail.com")
+    subject7: str = "Breakin Hearts!"
+    body7: str = "I'm killin it, romantically\n\nSincerely,\nCupid"
+    mail7 = Mail(mailaddress7a, mailaddress7b, subject7, body7)
+
+    mailaddress8a = MailAddress("She", "hismail.com")
+    mailaddress8b = MailAddress("Him", "hismail.com")
+    subject8: str = "Internal Email!"
+    body8: str = "This one stays in the family!\n\nSincerely,\nShe"
+    mail8 = Mail(mailaddress8a, mailaddress8b, subject8, body8)
+
+    inbox4 = []
+    outbox4 = [mail7, mail8]
+    mailaccount4= MailAccount("She", inbox4, outbox4)
+
     mailserver1 = MailServer("mymail.com", [mailaccount1])
     mailserver2 = MailServer("yourmail.com", [mailaccount2])
-    mailserver3 = MailServer("hismail.com", [mailaccount3])
+    mailserver3 = MailServer("hismail.com", [mailaccount3, mailaccount4])
 
     grand_ser_list = [mailserver1, mailserver2, mailserver3]
 
-    print(show_mail_account(mailaccount1))
-    input("press any key to continue. >")
-    print(show_mail_account(mailaccount2))
-    input("press any key to continue. >")
-    print(show_mail_account(mailaccount3))
-    input("press any key to continue. >")
+    for x in grand_ser_list:
+        print(show_mail_server(x))
+        input("press any key to continue. >")
     print("""
 
 ++++++++++++++++++++++++++++++++++++++++
@@ -248,9 +271,6 @@ if __name__ == "__main__":
 ++++++++++++++++++++++++++++++++++++++++
 """)
     deliver_all_mail(grand_ser_list)
-    print(show_mail_account(mailaccount1))
-    input("press any key to continue. >")
-    print(show_mail_account(mailaccount2))
-    input("press any key to continue. >")
-    print(show_mail_account(mailaccount3))
-    input("press any key to continue. >")
+    for x in grand_ser_list:
+        print(show_mail_server(x))
+        input("press any key to continue. >")
