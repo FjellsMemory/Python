91733e795ce725e0c27e9a2d47a8820758f67ee2
FjellsMemory <dg239@students.uni-freiburg.de>
2021-12-15 06:55:46 +0100

    updated espresso

--- mycode/espresso.py
+++ mycode/espresso.py
@@ -2,8 +2,20 @@
 from dataclasses import dataclass
 
 
+@dataclass
+class Bev():
+    """Class to represent an espresso drink."""
+
+    name: str = ""
+    price: float = 0.0
+    esp_num: int = 1
+    ppe: float = 0.0
+
+
 def total_cost(pp_drink: float, esp_in_drink: int) -> float:
     """
+    Total cost calculation.
+
     Take price of drink and # of esp in drink, subtract off price of the free
     double earned after buying 10 of them.
     """
@@ -11,27 +23,25 @@ def total_cost(pp_drink: float, esp_in_drink: int) -> float:
     return total
 
 
-@dataclass
-class Bev():
-    name: str = ""
-    price: float = 0.0
-    esp_num: int = 1
-    ppe: float = 0.0
+def create_bev(bev: Bev) -> Bev:
+    """Add attributes to bev instance."""
+    print("\nNew Drink...")
+    bev.name = input("Name of drink: ")
+    bev.price = float(input("It's price: "))
+    bev.esp_num = int(input("Number of espressos in dis bev? "))
+    return bev
 
 
 if __name__ == "__main__":
     bev1, bev2 = Bev(), Bev()
 
-    bev1.name = input("Name of first drink: ")
-    bev1.price = float(input("It's price: "))
-    bev1.esp_num = int(input("Number of espressos in dis bev? "))
-
-    bev2.name = input("Name of second drink: ")
-    bev2.price = float(input("It's price: "))
-    bev2.esp_num = int(input("Number of espressos in dis bev? "))
+    bev1 = create_bev(bev1)
+    bev2 = create_bev(bev2)
 
     bev1.ppe = total_cost(bev1.price, bev1.esp_num)
     bev2.ppe = total_cost(bev2.price, bev2.esp_num)
+    print("\n")
+
     print(f"Cost per esp in {bev1.name} = {bev1.ppe}")
     print(f"Cost per esp in a {bev2.name} = {bev2.ppe}")
 
