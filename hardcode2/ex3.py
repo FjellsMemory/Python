6a92330df8e55650982bba658892a26a8eb94aed
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-03 14:31:46 +0100

    movin and groovin

--- hardcode2/ex3.py
+++ hardcode2/ex3.py
@@ -1,11 +1,13 @@
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6")
+print("Hens", 25 + 30 / 6)  # Division results in float :)
 print("Roosters", 100 - 25 * 3 % 4)
 
 print("Now I will count the eggs:")
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)  # PEMDAS is the order of operations.
+# Remember that in PEMDAS, mult and div are done simultaneously from left to
+# right.  Same is true for addition and subtraction.
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
