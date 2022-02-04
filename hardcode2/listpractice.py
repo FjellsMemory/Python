a1ee600556e9708f2f2ee6f9d1105217e36bbc86
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-04 21:56:21 +0100

    workin with lists, boi

--- hardcode2/listpractice.py
+++ hardcode2/listpractice.py
@@ -1,24 +1,19 @@
-# myType = int or float or str or bool
+"""Removing element from list, list comprehension, more to come..."""
+
 
 def remove_letter(mylist: list, myletter: str) -> list:
     """Remove an element from a list."""
     try:
         while True:
-            print(myletter in mylist)
             mylist.remove(myletter)
-            print("i'm in da removing block")
     except ValueError:
         pass
     return mylist
 
 
-usrstr = input("gimme some txt, boi:  ")
-strlist = [x for x in usrstr]  # convert txt to list of single chars
-strlist.sort()  # group single letters together in list
-
-usrletter = input(f"here's your text in a list.\n{strlist}\n\
-pick a letter from it:  ")
-
+strlist = [x for x in (input("gimme some txt, boi:  "))]
+strlist.sort()
+usrletter = input(f"{strlist}\npick a letter from this alphabet soup:  ")
 strlist = remove_letter(strlist, usrletter)
 
 print("list after removing your selection:\n", strlist)
