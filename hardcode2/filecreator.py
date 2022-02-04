99cca49d3d4267311545f65ed343a37182b570b5
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-04 12:04:25 +0100

    creating a big mess of files

---
+++ hardcode2/filecreator.py
@@ -0,0 +1,9 @@
+print("K, this script is supposed to create new files.")
+while True:
+    filename = input("Create file (name):  ")
+    if filename.lower() == "quit":
+        break
+    open(f"c:\\Users\\fjell\\Documents\\Python\\hardcode2\\{filename}", 'x')
+
+# in this short script i see the benefit of creating the variable on which a
+# while loop will break or continue) INSIDE the while True loop.
