af11d22e6d9b17741d1bbf2e9aa438d9cb260f3e
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-04 12:05:50 +0100

    doin it

--- hardcode2/ex12.py
+++ hardcode2/ex12.py
@@ -1,9 +1,9 @@
-"""age = input("How old are you? >")
+age = input("How old are you? >")
 height = input("How tall are you? >")
 weight = input("How much do you weigh? >")
 
 print(f"So you're {age} old, {height} tall and {weight} heavy.")
-"""
+
 with open("c:\\Users\\fjell\\Documents\\Python\\hardcode2\\ex12.txt", mode='w')\
         as x:
     x.write(input("write your new sheez to da file:  >"))
