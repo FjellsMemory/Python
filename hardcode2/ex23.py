f829d7167373714276b7c23cd39d61fa4b9f107b
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-05 18:46:41 +0100

    workin ex23.py

--- hardcode2/ex23.py
+++ hardcode2/ex23.py
@@ -1,5 +1,3 @@
-"""This script is to acquaint one with strings n shit."""
-
 import sys  # far as i know i'm still only using argv from sys, as sys.argv
 script, encoding, error = sys.argv  # encoding still opaque, as is error
 
@@ -23,3 +21,30 @@ def print_line(line, encoding, errors):
 languages = open("languages.txt", encoding="utf-8")
 
 main(languages, encoding, error)
+
+"""This script is to acquaint one with strings n shit."""
+
+
+"""Small history lesson in encoding, ASCII is 8-bit encoding, only works for
+English because of 256 possibilities - each letter gets a binary number between
+0 and 255 so 00000000 - 11111111.  Unicode was the world's solution and is 
+32-bit but thats 4 billion characters and there is a lot of wasted space.  The
+smart solution is to encode most shit in 8-bit and then jump out to higher
+orders (16- or 32-bit) where needed.  UTF-8, Universal Transformation Format.
+
+b'\xe6\x96\x87\xe8\xa8\x80'
+
+here's a string of raw bytes for example (the b is for bytes).  what table of
+characters do you want to use to decode this string of bytes?  how's about the
+UTF-8 table?  
+
+raw_bytes = b'\xe6\x96\x87\xe8\xa8\x80'
+now...
+raw_bytes.decode()
+boom - 文言
+it decoded the byte string using the UTF-8 table
+just like 
+'文言'.encode() gives you...
+b'\xe6\x96\x87\xe8\xa8\x80'
+
+"""
