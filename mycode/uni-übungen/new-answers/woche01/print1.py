0d0a929bb71fb06cc1cc13534ac4d321d60c13f4
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-07 20:36:02 +0100

    movin on with uni exercises

--- mycode/uni-übungen/new-answers/woche01/print1.py
+++ mycode/uni-übungen/new-answers/woche01/print1.py
@@ -1,1 +1,3 @@
+"""This script prints Test 3 times on 3 successive lines."""
+
 print("Test\n" * 3)
--- mycode/uni-übungen/new-answers/woche01/print2.py
+++ mycode/uni-übungen/new-answers/woche01/print2.py
@@ -1,1 +1,3 @@
+"""This script prints 000 on three successive lines."""
+
 print(("0" * 3 + "\n") * 3)
