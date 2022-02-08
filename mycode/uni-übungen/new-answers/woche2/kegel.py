b2704c6c9ea28f95bfe9ea6d14b0bd8e924e5eac
FjellsMemory <dg239@students.uni-freiburg.de>
2022-02-08 11:39:26 +0100

    kegel.py erledigt

--- mycode/uni-übungen/new-answers/woche2/kegel.py
+++ mycode/uni-übungen/new-answers/woche2/kegel.py
@@ -1,6 +1,8 @@
 """Calculate the surface area of a cone."""
+from math import pi, sqrt
 
-radius = input("Radius: ")
-height = input("Höhe: ")
+radius = float(input("Radius: "))
+height = float(input("Höhe: "))
 
-print("Mantelfläche: ")
+area = (pi * radius) * sqrt(radius ** 2 + height ** 2)
+print("Mantelfläche:", round(area, 2))
