b4513b3eb8142a09002e9e6679a5ce63b977738e
FjellsMemory <dg239@students.uni-freiburg.de>
2022-05-26 05:52:55 +0200

    begun abrunden.py

---
+++ mycode/uni-übungen/new-answers/woche02/abrunden.py
@@ -0,0 +1,7 @@
+"""This script squares a number and then rounds it off 3 different ways."""
+
+usrinp = float(input("Kommazahl: "))
+print(f"Quadriert: {usrinp * usrinp}")
+print(f"Methode 1: {2}")
+print(f"Methode 2: {2}")
+print(f"Methode 3: {2}")
