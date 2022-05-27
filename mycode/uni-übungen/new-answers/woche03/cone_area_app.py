8f1d8958d36bb3765d836eb89b3905be37dbf8b2
FjellsMemory <dg239@students.uni-freiburg.de>
2022-05-27 09:58:45 +0200

    finished lib and app scripts of cone_area Aufgabe

--- mycode/uni-übungen/new-answers/woche03/cone_area_app.py
+++ mycode/uni-übungen/new-answers/woche03/cone_area_app.py
@@ -1,5 +1,7 @@
 """This script contains the application of cone_area."""
 
-from cone_area_lib import cone_area()
+from cone_area_lib import cone_area
 if __name__ == "__main__":
-    
+    r = float(input("Radius: "))
+    h = float(input("Höhe: "))
+    print(f"Mantelfläche: {cone_area(r, h)}")
