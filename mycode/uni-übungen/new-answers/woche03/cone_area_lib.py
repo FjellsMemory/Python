d5e089a0965783c8e13ff73520a540dbc54a0974
FjellsMemory <dg239@students.uni-freiburg.de>
2022-05-27 09:21:09 +0200

    deleting previous answers :D

--- mycode/uni-übungen/new-answers/woche03/cone_area_lib.py
+++
@@ -1,8 +0,0 @@
-"""This is the library for a func caluclating the linear area of a cone."""
-
-from math import pi, sqrt
-
-
-def cone_area(radius: float, height: float):
-    """Calculate the linear area of a cone."""
-    return round(float((pi * radius) * sqrt(radius ** 2 + height ** 2)), 2)
